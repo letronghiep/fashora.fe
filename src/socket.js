6187f7724ff7e2a9a7e306ea68e37aec50a0a026
Le Trong Hiep <letronghiep213@gmail.com>
2025-05-06 23:14:58 +0700

    change env socket

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
-
+const url = import.meta.env.NODE_ENV === 'pro' ? undefined : import.meta.env.VITE_SOCKET_URL
 export const socket = io(import.meta.env.VITE_SOCKET_URL, {
     transports: ['websocket', 'polling'],
     secure: true
