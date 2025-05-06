e877af5eb564f8f70cec95dbf9054a3c2f5dd244
Le Trong Hiep <letronghiep213@gmail.com>
2025-05-07 00:21:13 +0700

    allow auto connect socket

--- src/socket.js
+++ src/socket.js
@@ -1,3 +1,3 @@
 import { io } from 'socket.io-client';
 // const url = import.meta.env.NODE_ENV === 'pro' ? undefined : import.meta.env.VITE_SOCKET_URL
-export const socket = io(import.meta.env.VITE_SOCKET_URL, { autoConnect: false });
+export const socket = io(import.meta.env.VITE_SOCKET_URL);
