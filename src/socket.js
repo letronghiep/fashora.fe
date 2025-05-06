e9d23197bfbdd09caabcf1e52d92d219154238d3
Le Trong Hiep <letronghiep213@gmail.com>
2025-05-06 23:21:56 +0700

    autoConnect false for socket

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,3 @@
 import { io } from 'socket.io-client';
 const url = import.meta.env.NODE_ENV === 'pro' ? undefined : import.meta.env.VITE_SOCKET_URL
-export const socket = io(url, {
-    transports: ['websocket', 'polling'],
-    secure: true
-});
+export const socket = io(url, { autoConnect: false });
